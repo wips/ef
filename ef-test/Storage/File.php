99727839d1db152bc0e4e12575d39fa9dc7b0cae
Viktor Molokostov <Viktor_Molokostov@epam.com>
2014-03-23 01:18:44 +0200

    reallign

--- ef-test/Storage/File.php
+++ ef-test/Storage/File.php
@@ -1,33 +1,34 @@
 <?php
-namespace Ef\Storage;
-use Gaufrette\Filesystem;
+namespace Ef\Storage {
+    use Ef\Storage;
+    use Gaufrette\Filesystem;
 
-class File extends \Ef\Storage {
-    private $fs;
+    class File extends Storage {
+        private $fs;
 
-    function __construct(Filesystem $fs) {
-        $this->fs = $fs;
-    }
-
-    function getSavedPagesNumber() {
-        $content = $this->fs->read(EF_STORE_FILE);
-        return sizeof(unserialize($content)) / EF_IMG_PER_PAGE;
-    }
-
-    function save(array $data) {
-        $contents = $this->fs->read(EF_STORE_FILE);
-
-        if ($contents == '') {
-            $toSave = $data;
-        } else {
-            $toSave = array_merge(unserialize($contents), $data);
+        function __construct(Filesystem $fs) {
+            $this->fs = $fs;
         }
 
-        $this->fs->write(EF_STORE_FILE, serialize($toSave), true);
-    }
+        function getSavedPagesNumber() {
+            $content = $this->fs->read(EF_STORE_FILE);
+            return sizeof(unserialize($content)) / EF_IMG_PER_PAGE;
+        }
 
-    function items(array $filter) {
+        function save(array $data) {
+            $contents = $this->fs->read(EF_STORE_FILE);
 
+            if ($contents == '') {
+                $toSave = $data;
+            } else {
+                $toSave = array_merge(unserialize($contents), $data);
+            }
+
+            $this->fs->write(EF_STORE_FILE, serialize($toSave), true);
+        }
+
+        function items(array $filter) {
+
+        }
     }
 }
- 
